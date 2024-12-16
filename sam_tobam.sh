f2c8dd3fbb49c9be92c50b26e87bdc45c5f35ea2
biol726313 <mku8778@utulsa.edu>
2024-12-16 10:16:47 -0600

    Add files via upload

---
+++ sam_tobam.sh
@@ -0,0 +1,1 @@
+samtools view -bS /scratch/biol726313/BIOL7263_Genomics/sequencing_data/ecoli/assembly/mapping_to_assembly/contigs_mapped.sam >  /scratch/biol726313/BIOL7263_Genomics/sequencing_data/ecoli/assembly/mapping_to_assembly/contigs_mapped.bam
